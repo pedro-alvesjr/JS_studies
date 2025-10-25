8a73154361ae33baa40c2bfcf668875292eecdb7
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-10-25 00:08:07 -0300

    Create exercise002.js

---
+++ exercise002.js
@@ -0,0 +1,18 @@
+let a = 20
+let b = 10
+
+let sum = (a + b)
+let subtraction = (a - b)
+let multiplying = (a * b)
+let division = (a / b)
+let rest = (a % b)
+let exp = (a ** b)
+
+console.log(
+    `The sum of ${a} and ${b} is ${sum}\n`,
+    `The subtraction of ${a} and ${b} is ${subtraction}\n`,
+    `The multiplication of ${a} and ${b} is ${multiplying}\n`,
+    `The division of ${a} and ${b} is ${division}\n`,
+    `The rest of the division of ${a} and ${b} is ${rest}\n`,
+    `The exponential of ${a} and ${b} is ${exp}`
+)
