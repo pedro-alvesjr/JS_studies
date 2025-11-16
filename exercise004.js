83d7a94abd78527cbd2fb068adfec5237e8dd748
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-11-16 02:05:50 -0300

    Create exercise004.js

---
+++ exercise004.js
@@ -0,0 +1,25 @@
+let dia = 2
+
+switch (dia) {
+    case 1:
+        console.log("Hoje é domingo.")
+        break;
+    case 2:
+        console.log("Hoje é segunda-feira.")
+        break;
+    case 3:
+        console.log("Hoje é terça-feira.")
+        break;
+    case 4:
+        console.log("Hoje é quarta-feira.")
+        break;
+    case 5:
+        console.log("Hoje é quinta-feira.")
+        break;
+    case 6:
+        console.log("Hoje é sexta-feira.")
+        break;
+    case 7:
+        console.log("Hoje é sábado.")
+        break;    
+};
